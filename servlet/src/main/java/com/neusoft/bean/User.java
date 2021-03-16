d11f3c195e9457864b40b4ab8c947ce0c41b27ae
Ninelie_Z <ninelie_z@outlook.com>
2021-03-16 15:46:33 +0800

    create User

---
+++ servlet/src/main/java/com/neusoft/bean/User.java
@@ -0,0 +1,37 @@
+package com.neusoft.bean;
+
+public class User {
+    int id;
+    String userName;
+    String password;
+    String telphone;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getTelphone() {
+        return telphone;
+    }
+    public void setTelphone(String telphone) {
+        this.telphone = telphone;
+    }
+}
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", userName=" + userName + ", password=" + password + ", telphone=" + telphone + "]";
+    }
--- servlet/src/main/java/com/neusoft/util/DBUtil.java
+++ servlet/src/main/java/com/neusoft/util/DBUtil.java
@@ -6,7 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import javax.naming.spi.DirStateFactory.Result;
 
 public class DBUtil {
     public Connection getConnection() {

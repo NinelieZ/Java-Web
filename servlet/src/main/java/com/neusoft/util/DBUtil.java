22a40c73568d9f1c18eb724b6054813d8de7e937
Ninelie_Z <ninelie_z@outlook.com>
2021-03-16 14:04:51 +0800

    create DBUtil

---
+++ servlet/src/main/java/com/neusoft/util/DBUtil.java
@@ -0,0 +1,52 @@
+public import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+public class DBUtil {
+    public Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql:///servlet", "root", "225874");
+            System.out.println("连接成功");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public void DBclose(Connection conn, PreparedStatement pstm, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (pstm != null) {
+            try {
+                pstm.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DBUtil db = new DBUtil();
+        db.getConnection()
+    }
+}
